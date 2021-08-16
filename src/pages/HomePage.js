0b739b25d4b45600d26e99914c97658f2111e800
Imran Hossain <ih060899@users.noreply.github.com>
2021-08-16 11:24:14 +0600

    add isPending in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,16 +4,19 @@ import './HomePage.scss';
 
 const HomePage = () => {
     const [teams, setTeams] = useState([]);
+    const [isPending, setIsPending] = useState(true);
     useEffect(() => {
         const fetchAllTeams = async () => {
             const response = await fetch(`${process.env.REACT_APP_ROOT_URL}/team`);
             const data = await response.json();
             setTeams(data);
+            setIsPending(false);
         };
         fetchAllTeams();
     }, []);
     return (
         <div className='HomePage'>
+            {isPending && <div>Loading...</div>}
             <div className='header-section'>
                 <h1 className='app-name'>IPL Dashboard</h1>
             </div>
